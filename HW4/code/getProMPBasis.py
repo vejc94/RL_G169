deda88671ff3be70bf85e895bb8c3e67eec79ea3
victorejc94 <vicernesto@hotmail.com>
2021-01-31 10:21:33 +0100

    victors zeug was nur bis zur f funktioniert

--- HW4/code/getProMPBasis.py
+++ HW4/code/getProMPBasis.py
@@ -2,9 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def getProMPBasis(dt, nSteps, n_of_basis, bandwidth):
-
+    
     time = np.arange(dt,nSteps*dt,dt)
+    tau = 1
+    alpha_z = 3/time[-1]
+    low = -2*bandwidth
+    high = time[-1] + 2*bandwidth
+    
+    C = np.zeros(n_of_basis)  # Basis function centres
+    H = np.zeros(n_of_basis)  # Basis function bandwidths
+    
+    C= np.linspace(-2*bandwidth, time[-1] + 2*bandwidth, n_of_basis)
 
-    Phi = 0
-
+    for i in range(n_of_basis):
+        H[i] = bandwidth ** (1 / 4)
+    H[:] = bandwidth
+    
+    z = np.exp(-alpha_z*tau*time)
+    
+    Phi = np.zeros((nSteps, n_of_basis))    
+    
+    for k in range(nSteps):
+        for j in range(n_of_basis):
+            Phi[k,j] = np.exp(-1/2*( time[k] - C[j] )**2/H[j] ) # Basis function activation over time
+    
+    for k in range(nSteps):    
+        Phi[k,:] = (Phi[k,:]) / np.sum( Phi[k,:]) # Normalize basis functions and weight by canonical state
+        
+    plot_proMP(Phi, n_of_basis, time)
     return Phi
+
+def plot_proMP(Phi, n_of_basis, time):
+    for i in range(n_of_basis):
+        plt.plot(time, Phi[:, i])
+
+        
+    
+    
--- HW4/code/proMP.py
+++ HW4/code/proMP.py
@@ -12,15 +12,17 @@ def proMP (nBasis, condition=False):
     q = data[0]
     qd = data[1]
     qdd = data[2]
-
     bandwidth = 0.2
+    
     Phi = getProMPBasis( dt, nSteps, nBasis, bandwidth )
-
-    w = ...
-    mean_w = ...
-    cov_w = ...
-    mean_traj = ...
-    std_traj = ...
+    
+    pseudo_inv = np.linalg.inv(Phi.T@Phi)
+    w = pseudo_inv@Phi.T@q.T
+    mean_w = np.mean(w, axis=1)
+    cov_w = np.cov(w, rowvar=True)
+    mean_traj = Phi@mean_w
+    
+    std_traj = np.sqrt(np.diag(0.01*np.eye(nSteps) + Phi@cov_w@Phi.T))
 
     plt.figure()
     plt.fill_between(time, mean_traj - 2*std_traj, mean_traj + 2*std_traj, alpha=0.5, edgecolor='#1B2ACC', facecolor='#089FFF')
@@ -32,14 +34,19 @@ def proMP (nBasis, condition=False):
     if condition:
         y_d = 3
         Sig_d = 0.0002
-        t_point = np.round(2300/2)
+        t_point = np.round(2300/2).astype(np.int)
 
-        tmp = np.dot(cov_w, Phi[:,t_point]) / (Sig_d + np.dot(Phi[:,t_point].T,np.dot(cov_w,Phi[:,t_point])))
+        tmp = np.dot(cov_w, Phi[t_point,:]) / (Sig_d + np.dot(Phi[t_point,:].T,np.dot(cov_w,Phi[t_point,:])))
 
-        cov_w_new = ...
-        mean_w_new = ...
-        mean_traj_new = ...
-        std_traj_new = ...
+        cov_w_new = cov_w - tmp@Phi[t_point,:].T * cov_w
+        mean_w_new = mean_w + tmp*(y_d - Phi[t_point,:].T@mean_w)
+        
+        traj_new = np.zeros((10, 1499))
+        for i in range(10):
+            w_new = np.random.normal(mean_w_new, np.std(np.diagonal(cov_w_new)))
+            traj_new[i] = Phi@w_new
+        mean_traj_new = np.mean(traj_new, axis=0)
+        std_traj_new = np.std(traj_new, axis=0)
 
         plt.figure()
         plt.fill_between(time, mean_traj - 2*std_traj, mean_traj + 2*std_traj, alpha=0.5, edgecolor='#1B2ACC', facecolor='#089FFF')
@@ -47,8 +54,8 @@ def proMP (nBasis, condition=False):
         plt.fill_between(time, mean_traj_new - 2*std_traj_new, mean_traj_new + 2*std_traj_new, alpha=0.5, edgecolor='#CC4F1B', facecolor='#FF9848')
         plt.plot(time, mean_traj_new, color='#CC4F1B')
 
-        sample_traj = np.dot(Phi.T,np.random.multivariate_normal(mean_w_new,cov_w_new,10).T)
-        plt.plot(time,sample_traj)
+        sample_traj = np.dot(Phi, np.random.multivariate_normal(mean_w_new,cov_w_new,10).T)
+        plt.plot(time, sample_traj)
         plt.title('ProMP after contidioning with new sampled trajectories')
 
     plt.draw_all()
