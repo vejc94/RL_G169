afd97179dd073e140da4dcc3c5fb0b57faf11c8d
Daniel <piendl.daniel@gmail.com>
2021-03-02 20:56:12 +0100

    Final HW5

--- HW5/Policy_Gradient/policy_gradient.py
+++ HW5/Policy_Gradient/policy_gradient.py
@@ -2,48 +2,111 @@ from matplotlib import pyplot as plt
 import numpy as np
 from Pend2dBallThrowDMP import *
 import time
+
 # %matplotlib inline
 np.set_printoptions(precision=3, linewidth=100000)
-
 
 env = Pend2dBallThrowDMP()
 
 numDim = 10
 numSamples = 25
-maxIter = 10
+maxIter = 100
 numTrials = 10
 
+
 # YOUR CODE HERE
-from gaussian_functions import *
-mu = np.zeros(numDim)
-sigma = np.diag((np.ones(10)))
-alpha = 0.1
 
-# reward = np.zeros((maxIter, numSamples))
-# for i in range(maxIter):
-#     gradient_mu = np.zeros(mu.shape)
-#     for j in range(numSamples):
-#         theta = np.random.multivariate_normal(mu, sigma)
-#         reward[i,j] = env.getReward(theta)
-#         gradient_mu += multivariate_gaussian_log_gradient(theta, mu, sigma)[0] * reward[i,j]
-#
-#     mu = mu + alpha * gradient_mu
+def log_gauss_derivative(x, mu, sigma):
+    sigma_inv = np.diag(1 / np.diag(sigma))
+    diff = x - mu
+    der_mu = sigma_inv.dot(diff)
+    der_sigma = 0.5 * (-sigma_inv + sigma_inv.dot(np.outer(diff, diff)).dot(sigma_inv))
+
+    return der_mu, der_sigma
 
 
-reward = np.zeros((maxIter, numSamples))
-for i in range(maxIter):
-    gradient_mu = np.zeros(mu.shape)
-    theta = [None] * numSamples
-    for j in range(numSamples):
-        theta[j] = np.random.multivariate_normal(mu, sigma)
-        reward[i,j] = env.getReward(theta[j])
+def exploration(alpha, use_baseline=True, change_sigma=False, sigma_lb=None):
+    if change_sigma:
+        assert sigma_lb is not None
 
-    b = np.mean(reward[i, :])
-    for j in range(numSamples):
-        gradient_mu += multivariate_gaussian_log_gradient(theta[j], mu, sigma)[0] * (reward[i,j] - b)
+    assert isinstance(alpha, tuple)
 
-    mu = mu + alpha * gradient_mu
+    fig0, ax0 = plt.subplots()
 
-fig0, ax0 = plt.subplots()
-ax0.plot(np.arange(maxIter), [np.mean(reward[i]) for i in range(maxIter)])
-plt.show()
+
+    for alpha_i in alpha:
+        reward = np.zeros((numTrials, maxIter, numSamples))
+        for trial_i in range(numTrials):
+            mu = np.zeros(numDim)
+            sigma = np.diag((np.ones(10))) * 10 ** 2
+            for i in range(maxIter):
+                gradient_mu = np.zeros(mu.shape)
+                gradient_sigma = np.zeros(sigma.shape)
+                theta = [np.zeros(10)] * numSamples
+                for j in range(numSamples):
+                    theta[j] = np.random.multivariate_normal(mu, sigma)
+                    reward[trial_i, i, j] = env.getReward(theta[j])
+
+                assert id(theta[0]) != id(theta[1])
+                if use_baseline:
+                    b = np.mean(reward[trial_i, i, :])
+                else:
+                    b = 0
+
+                for j in range(numSamples):
+                    derivative_gaussian = log_gauss_derivative(theta[j], mu, sigma)
+                    gradient_mu += derivative_gaussian[0] * (reward[trial_i, i, j] - b)
+                    if change_sigma:
+                        gradient_sigma += derivative_gaussian[1] * (reward[trial_i, i, j] - b)
+
+                mu = mu + alpha_i * gradient_mu / numSamples
+
+                # Learn covariance
+                if change_sigma:
+                    gradient_sigma = np.diag(np.diag(sigma))  # Only use elements on diagonal
+                    sigma = sigma + alpha_i * gradient_sigma / numSamples  # Normalize by number of samples
+
+                    # Set all diagonal elements below lower bound to the lower bound
+                    sigma[range(numDim), range(numDim)] = np.maximum(sigma[range(numDim), range(numDim)], sigma_lb)
+
+        reward_mean = np.mean(reward, axis=(0, 2))
+        reward_std = np.empty(reward_mean.shape)
+        for i in range(maxIter):
+            reward_std[i] = np.sqrt(np.sum((reward[:, i, :].ravel() - reward_mean[i]) ** 2) / (numTrials * numSamples))
+
+        ax0.plot(np.arange(maxIter), reward_mean, label=f"Alpha: {alpha_i}")
+        plt.fill_between(np.arange(maxIter), reward_mean - 2 * reward_std, reward_mean + 2 * reward_std, alpha=0.5,
+                         edgecolor='#1B2ACC', facecolor='#089FFF')
+
+    # ax0.set_title("Subtracted baseline policy gradient - Variance learned")
+    ax0.set_xlabel("Number of iterations")
+    ax0.set_ylabel("Mean return for all runs")
+    ax0.legend()
+    ax0.grid()
+    plt.tight_layout()
+
+    return fig0
+
+
+fig = exploration(alpha=(0.1,), use_baseline=False, change_sigma=False)
+plt.gca().set_title("Normal policy gradient")
+plt.tight_layout()
+fig.savefig("Normal policy gradient.pdf")
+print("si")
+
+fig = exploration(alpha=(0.1,), use_baseline=True, change_sigma=False)
+plt.gca().set_title("Subtracted baseline policy gradient")
+plt.tight_layout()
+fig.savefig("Subtracted baseline policy gradient.pdf")
+print("si")
+
+fig = exploration(alpha=(0.1, 0.2, 0.4), use_baseline=True, change_sigma=False)
+plt.gca().set_title("Subtracted baseline policy gradient\nVarying alpha")
+plt.tight_layout()
+fig.savefig("Subtracted baseline policy gradient_Varying alpha.pdf")
+print("si")
+
+fig = exploration(alpha=(0.4,), use_baseline=True, change_sigma=True, sigma_lb=1)
+plt.gca().set_title("Subtracted baseline policy gradient\nVariance learned")
+plt.tight_layout()
+fig.savefig("Subtracted baseline policy gradient_Variance learned.pdf")
--- HW5/Reinforcement_Learning/gridWorld.py
+++ HW5/Reinforcement_Learning/gridWorld.py
@@ -1,36 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import warnings
+
 warnings.filterwarnings("ignore")
+
+maxIters = 15
 
 
 ##
 def genGridWorld():
     O = -1e5  # Dangerous places to avoid
-    D = 35    # Dirt
+    D = 35  # Dirt
     W = -100  # Water
-    C = -3000 # Cat
+    C = -3000  # Cat
     T = 1000  # Toy
-    grid_list = {0:'', O:'O', D:'D', W:'W', C:'C', T:'T'}
+    grid_list = {0: '', O: 'O', D: 'D', W: 'W', C: 'C', T: 'T'}
     grid_world = np.array([[0, O, O, 0, 0, O, O, 0, 0, 0],
-        [0, 0, 0, 0, D, O, 0, 0, D, 0],
-        [0, D, 0, 0, 0, O, 0, 0, O, 0],
-        [O, O, O, O, 0, O, 0, O, O, O],
-        [D, 0, 0, D, 0, O, T, D, 0, 0],
-        [0, O, D, D, 0, O, W, 0, 0, 0],
-        [W, O, 0, O, 0, O, D, O, O, 0],
-        [W, 0, 0, O, D, 0, 0, O, D, 0],
-        [0, 0, 0, D, C, O, 0, 0, D, 0]])
+                           [0, 0, 0, 0, D, O, 0, 0, D, 0],
+                           [0, D, 0, 0, 0, O, 0, 0, O, 0],
+                           [O, O, O, O, 0, O, 0, O, O, O],
+                           [D, 0, 0, D, 0, O, T, D, 0, 0],
+                           [0, O, D, D, 0, O, W, 0, 0, 0],
+                           [W, O, 0, O, 0, O, D, O, O, 0],
+                           [W, 0, 0, O, D, 0, 0, O, D, 0],
+                           [0, 0, 0, D, C, O, 0, 0, D, 0]])
     return grid_world, grid_list
 
 
 ##
 def showWorld(grid_world, tlt):
     fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
+    ax = fig.add_subplot(1, 1, 1)
     ax.set_title(tlt)
-    ax.set_xticks(np.arange(0.5,10.5,1))
-    ax.set_yticks(np.arange(0.5,9.5,1))
+    ax.set_xticks(np.arange(0.5, 10.5, 1))
+    ax.set_yticks(np.arange(0.5, 9.5, 1))
     ax.grid(color='b', linestyle='-', linewidth=1)
     ax.imshow(grid_world, interpolation='nearest', cmap='copper')
     return ax
@@ -40,133 +43,145 @@ def showWorld(grid_world, tlt):
 def showTextState(grid_world, grid_list, ax):
     for x in range(grid_world.shape[0]):
         for y in range(grid_world.shape[1]):
-            if grid_world[x,y] >= -3000:
-                ax.annotate(grid_list.get(grid_world[x,y]), xy=(y,x), horizontalalignment='center')
+            if grid_world[x, y] >= -3000:
+                ax.annotate(grid_list.get(grid_world[x, y]), xy=(y, x), horizontalalignment='center')
 
 
 ##
 def showPolicy(policy, ax):
     for x in range(policy.shape[0]):
         for y in range(policy.shape[1]):
-            if policy[x,y] == 0:
-                ax.annotate('$\downarrow$', xy=(y,x), horizontalalignment='center')
-            elif policy[x,y] == 1:
-                ax.annotate(r'$\rightarrow$', xy=(y,x), horizontalalignment='center')
-            elif policy[x,y] == 2:
-                ax.annotate(r'$\uparrow$', xy=(y,x), horizontalalignment='center')
-            elif policy[x,y] == 3:
-                ax.annotate('$\leftarrow$', xy=(y,x), horizontalalignment='center')
-            elif policy[x,y] == 4:
-                ax.annotate('$\perp$', xy=(y,x), horizontalalignment='center')
+            if policy[x, y] == 0:
+                ax.annotate('$\downarrow$', xy=(y, x), horizontalalignment='center')
+            elif policy[x, y] == 1:
+                ax.annotate(r'$\rightarrow$', xy=(y, x), horizontalalignment='center')
+            elif policy[x, y] == 2:
+                ax.annotate('$\u2191$', xy=(y, x), horizontalalignment='center')
+            elif policy[x, y] == 3:
+                ax.annotate('$\leftarrow$', xy=(y, x), horizontalalignment='center')
+            elif policy[x, y] == 4:
+                ax.annotate('$\perp$', xy=(y, x), horizontalalignment='center')
 
 
 ##
-def ValIter(R, discount, maxSteps, infHor, probModel=None):
+def ValIter(R, discount=None, maxSteps=None, infHor=False, probModel=False):
     T = maxSteps
     rows, columns = R.shape
-    V = np.zeros([rows, columns, T])
-    V[:, :, -1] = np.copy(R)
-    Q = np.zeros([rows, columns, 5])
-    
+
+    robot_actions = {
+        "down": 0,
+        "right": 1,
+        "up": 2,
+        "left": 3,
+        "stay": 4
+    }
+
+    Q = np.zeros((rows, columns, len(robot_actions)))  # Basis Function
+
     if not infHor:
-        for i in range(T-2, -1, -1):
-            for k in range(rows):  # row
-                for l in range(columns):  # column
-                    for a in range(5):  # iterate over actions
+        V = np.zeros((rows, columns, T))                #Value Function
+        V[:, :, -1] = np.copy(R)
+
+        for t in reversed(range(T - 1)):
+            for row in range(rows):
+                for col in range(columns):
+                    for action in robot_actions.items():
                         if not probModel:
-                            new_state = trans_model(np.array([k, l]), a)
-                            Q[k, l, a] = R[k, l] + V[new_state[0], new_state[1], i+1]
-                       
+                            new_pos = calcNextPosition(action[0], np.asarray([row, col]))
+                            Q[row, col, action[1]] = R[row, col] + V[new_pos[0], new_pos[1], t + 1]
                         else:
-                            if a == 0 or a == 2:  # up or down
-                                new_st_1 = trans_model(np.array([k, l]), a)
-                                new_st_2 = trans_model(np.array([k, l]), 1)
-                                new_st_3 = trans_model(np.array([k, l]), 3)
-                                new_st_4 = trans_model(np.array([k, l]), 4)
-                                
-                                Q[k, l, a] = R[k, l] + 0.7*V[new_st_1[0], new_st_1[1], i+1]\
-                                            + 0.1*V[new_st_2[0], new_st_2[1], i+1]\
-                                            + 0.1*V[new_st_3[0], new_st_3[1], i+1]\
-                                            + 0.1*V[new_st_4[0], new_st_4[1], i+1]\
-                            
-                            elif a == 1 or a == 3:  # right or left
-                                new_st_1 = trans_model(np.array([k, l]), a)
-                                new_st_2 = trans_model(np.array([k, l]), 0)
-                                new_st_3 = trans_model(np.array([k, l]), 2)
-                                new_st_4 = trans_model(np.array([k, l]), 4)
-                                
-                                Q[k, l, a] = R[k, l] + 0.7*V[new_st_1[0], new_st_1[1], i+1]\
-                                            + 0.1*V[new_st_2[0], new_st_2[1], i+1]\
-                                            + 0.1*V[new_st_3[0], new_st_3[1], i+1]\
-                                            + 0.1*V[new_st_4[0], new_st_4[1], i+1]
-        
-                            elif a == 4:  # stay
-                                new_state = trans_model(np.array([k, l]), a)
-                                Q[k, l, a] = R[k, l] + V[new_state[0], new_state[1], i+1]
-                 
-            V[:,:, i] = np.max(Q, axis=2)
+                            #Task d)
+                            new_pos, prob = calcNextPosition(action[0], np.asarray([row, col]), True)
+                            Q[row, col, action[1]] = R[row, col]
+                            for pos, p in zip(new_pos, prob):
+                                Q[row, col, action[1]] = Q[row, col, action[1]] + p * V[pos[0], pos[1], t + 1]
+                    V[row, col, t] = max(Q[row, col, :])
     else:
-        diff = np.inf
-        V = np.copy(R)
-        V_old = np.zeros([rows, columns])
-        while np.any(np.abs(diff) > 0.1):
-            
-            for k in range(rows):  # row
-                for l in range(columns):  # column
-                    for a in range(5):  # iterate over actions
-                        new_state = trans_model(np.array([k, l]), a)
-                        Q[k, l, a] = R[k, l] + discount*V[new_state[0], new_state[1]]
+        improvement = np.inf
+        V = np.zeros(R.shape)
+        V_last = np.zeros(R.shape)
+
+        while improvement > 1e-5:
+            for row in range(rows):
+                for col in range(columns):
+                    for action in robot_actions.items():
+                        new_pos = calcNextPosition(action[0], np.asarray([row, col]))
+                        Q[row, col, action[1]] = R[row, col] + discount * V[new_pos[0], new_pos[1]]
             V = np.max(Q, axis=2)
-            diff = V - V_old
-            V_old = np.copy(V)
-            
+            improvement = abs(np.sum(V.flatten() - V_last.flatten()))
+            V_last = V
+
     return V, Q
+
 
 
 ##
 def maxAction(V, R, discount, probModel=None):
-    pass
+    # YOUR CODE HERE
+    pass        #TODO ?????
+
 
 ##
-def findPolicy(Q, probModel=None):
-    return np.argmax(Q, axis=2)
+def findPolicy(Q, probModel=False):
+    if not probModel:
+        return np.argmax(Q, axis=2)
+    else:
+        pass    #TODO ?????
+
+
+def calcNextPosition(action, pos, prob_model=False):
+    new_pos = np.zeros((1, 2))
+    prob = 0
+    if not prob_model:
+        if action == "down":
+            new_pos = pos + np.array([1, 0])
+            if new_pos[0] > 8:
+                new_pos = pos
+
+        elif action == "right":
+            new_pos = pos + np.array([0, 1])
+            if new_pos[1] > 9:
+                new_pos = pos
+
+        elif action == "up":
+            new_pos = pos + np.array([-1, 0])
+            if new_pos[0] < 0:
+                new_pos = pos
+
+        elif action == "left":
+            new_pos = pos + np.array([0, -1])
+
+            if new_pos[1] < 0:
+                new_pos = pos
+
+        elif action == "stay":
+            new_pos = pos
+    else:
+        if action == "down" or action == "up":
+            #also possible action left OR right OR failing
+            new_pos = calcNextPosition(action, pos)
+            new_pos = np.vstack((new_pos, calcNextPosition("left", pos)))
+            new_pos = np.vstack((new_pos, calcNextPosition("right", pos)))
+            new_pos = np.vstack((new_pos, calcNextPosition("stay", pos)))
+            prob = np.array([0.7, 0.1, 0.1, 0.1]).reshape((4, 1))
+        elif action == "right" or action == "left":
+            #possible actions up OR down OR failing
+            new_pos = calcNextPosition(action, pos)
+            new_pos = np.vstack((new_pos, calcNextPosition("down", pos)))
+            new_pos = np.vstack((new_pos, calcNextPosition("up", pos)))
+            new_pos = np.vstack((new_pos, calcNextPosition("stay", pos)))
+            prob = np.array([0.7, 0.1, 0.1, 0.1]).reshape((4, 1))
+        else:
+            new_pos = pos.reshape((1, 2))
+            prob = np.array([1])
+
+    if prob_model == False:
+        return np.asarray(new_pos)
+    else:
+        return np.asarray(new_pos), np.asarray(prob)
+
 
 ############################
-def trans_model(state, action, probModel=False):  
-    if not probModel:
-        if action == 0:  # down
-            if state[0] < 8: 
-                new_state = state + np.array([1, 0])
-            else: 
-                new_state = state
-        
-        elif action == 1:  # right
-            if state[1] < 9:
-                new_state = state + np.array([0, 1])
-            else:
-                new_state = state
-        
-        elif action == 3:  # left
-            if state[1] > 0:
-                new_state = state + np.array([0, -1])
-            else:
-                new_state = state
-        
-        elif action == 2:  # up
-            if state[0] > 0:
-                new_state = state + np.array([-1, 0]) 
-            else:
-                new_state = state
-                    
-        elif action == 4:  # stay
-            new_state = state
-        
-        else:
-            print('no action was given bip bup')
-            new_state = state
-    
-    return new_state
-
 
 saveFigures = True
 
@@ -174,17 +189,14 @@ data = genGridWorld()
 grid_world = data[0]
 grid_list = data[1]
 
-# YOUR CODE HERE
-probModel = ...
-
 ax = showWorld(grid_world, 'Environment')
 showTextState(grid_world, grid_list, ax)
 if saveFigures:
     plt.savefig('gridworld.pdf')
-
-# Finite Horizon
-V, Q = ValIter(grid_world, 0, 15, False)
-V = V[:,:,0];
+#
+# # Finite Horizon
+V, Q = ValIter(grid_world, maxSteps=15, infHor=False)
+V = V[:, :, 0]
 showWorld(np.maximum(V, 0), 'Value Function - Finite Horizon')
 if saveFigures:
     plt.savefig('value_Fin_15.pdf')
@@ -194,22 +206,22 @@ ax = showWorld(grid_world, 'Policy - Finite Horizon')
 showPolicy(policy, ax)
 if saveFigures:
     plt.savefig('policy_Fin_15.pdf')
-
-# Infinite Horizon
-V, Q= ValIter(grid_world, 0.8, 15, True)
+#
+# # # Infinite Horizon
+V, Q = ValIter(grid_world, discount=0.8, infHor=True)
 showWorld(np.maximum(V, 0), 'Value Function - Infinite Horizon')
 if saveFigures:
-    plt.savefig('value_Inf_08.pdf')
+   plt.savefig('value_Inf_08.pdf')
 
-policy = findPolicy(Q);
+policy = findPolicy(Q)
 ax = showWorld(grid_world, 'Policy - Infinite Horizon')
 showPolicy(policy, ax)
 if saveFigures:
     plt.savefig('policy_Inf_08.pdf')
 
-# Finite Horizon with Probabilistic Transition
-V, Q = ValIter(grid_world, 0, 15, infHor=False, probModel=True)
-V = V[:,:,0];
+# # Finite Horizon with Probabilistic Transition
+V, Q = ValIter(grid_world, maxSteps=15, probModel=True)
+V = V[:, :, 0]
 showWorld(np.maximum(V, 0), 'Value Function - Finite Horizon with Probabilistic Transition')
 if saveFigures:
     plt.savefig('value_Fin_15_prob.pdf')
@@ -218,4 +230,4 @@ policy = findPolicy(Q)
 ax = showWorld(grid_world, 'Policy - Finite Horizon with Probabilistic Transition')
 showPolicy(policy, ax)
 if saveFigures:
-    plt.savefig('policy_Fin_15_prob.pdf')
+   plt.savefig('policy_Fin_15_prob.pdf')
